f55ae9cc09855406e31ceb935cb6d67b03590bab
cockyconan <357515275@qq.com>
2019-11-10 23:59:22 +0800

    11-4 working on create view

--- app/src/main/java/student/jnu/cockyconan/itimer_copycat/CreateTimer.java
+++ app/src/main/java/student/jnu/cockyconan/itimer_copycat/CreateTimer.java
@@ -1,22 +1,73 @@
 package student.jnu.cockyconan.itimer_copycat;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.util.Locale;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class CreateTimer extends AppCompatActivity {
     private EditText title;
     private EditText note;
-
+    private Button saveBtn;
+    private Button cancelBtn;
+    private TextView txtDate ;
+    private MyDialog mMyDialog;
+    private DateFormat format= DateFormat.getDateTimeInstance();
+    private Calendar calendar= Calendar.getInstance(Locale.CHINA);
+    private View setloop_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_timer);
+        txtDate=(TextView)findViewById(R.id.create_timer_datetimelayout_showdate);
+        saveBtn = (Button) findViewById(R.id.create_timer_save_button);
+        cancelBtn =(Button) findViewById(R.id.create_timer_cancel_button);
 
+
+        //时间选择长按短按区分：
+        //！！！！！---------------------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------------------------------
+        //实现长按出现相对时间选择
+        View longtap=(View)findViewById(R.id.create_timer_date_time_layout);
+        longtap.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                Toast.makeText(view.getContext(),"长按时间设置", LENGTH_SHORT).show();
+                return true;//return true不响应短按事件
+            }
+        });
+        //单点又出现不同的选择
+        longtap.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+               // Toast.makeText(v.getContext(),"短按时间设置", LENGTH_SHORT).show();
+                showDatePickerDialog(CreateTimer.this,  0, txtDate, calendar);
+
+            }});
+        //-------------------------------------------------------------------------------------------------------------------------------------
+        //---------------------------------------------------------------------------------------------------------------------------------------
 
         title= (EditText)findViewById(R.id.create_timer_edittext_title);
         note =(EditText)findViewById(R.id.create_timer_edittext_note);
@@ -28,10 +79,138 @@ public class CreateTimer extends AppCompatActivity {
     {
         switch (which_is_clicked.getId())
         {
-            case R.id.create_timer_addpicture_layout:
-                Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+            case R.id.create_timer_addpicture_layout: {
+
+                Toast.makeText(this, "添加图片", LENGTH_SHORT).show();
                 break;
-                default: Toast.makeText(this, "登录failed", Toast.LENGTH_SHORT).show();
+            }
+            /*case R.id.create_timer_date_time_layout:
+            {
+                Toast.makeText(this, "设置时间", LENGTH_SHORT).show();
+                break;
+            }*/
+            case R.id.create_timer_repeat_layout:
+            {
+
+                setloop_view = getLayoutInflater().inflate(R.layout.create_timer_setloop_dialog, null);
+                mMyDialog = new MyDialog(this, 0, 0, setloop_view, R.style.DialogTheme);
+                mMyDialog.setCancelable(true);
+                mMyDialog.show();
+                //https://blog.csdn.net/qq_34882418/article/details/81085608
+                //https://blog.csdn.net/sakurakider/article/details/80735400
+                break;
+            }
+            case R.id.create_timer_cancel_button:
+            {
+                AlertDialog.Builder builder  = new AlertDialog.Builder(this);
+                builder.setTitle("Are you sure ?" ) ;
+                builder.setIcon(R.drawable.skull);
+                builder.setMessage("any written info will not be saved !" ) ;
+                builder.setPositiveButton("just do it !" ,  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intentswitch=new Intent(CreateTimer.this, MainActivity.class);
+                        startActivity(intentswitch);
+                        CreateTimer.this.finish();
+                    }
+                });
+                builder.setNegativeButton("misclicked !",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(CreateTimer.this,"that was close !",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
+
+                break;
+            }
+            case R.id.create_timer_save_button:
+            {
+                Toast.makeText(this, "保存按钮", LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.create_timer_setloopdialog_buttoncancel:
+            {
+                mMyDialog.dismiss();
+                break;
+            }
+            case R.id.create_timer_setloopdialog_buttonok://设置周期的确定按钮
+            {
+                //必须渲染来获取dialog中的控件
+
+
+                EditText edtxt=(EditText)setloop_view.findViewById(R.id.create_timer_setloop_value);
+                TextView txt=(TextView) findViewById(R.id.create_timer_repeat_show);
+
+
+                if(TextUtils.isEmpty(edtxt.getText()))
+                {
+                    Toast.makeText(this, "the value of the cycle can't be empty !", LENGTH_SHORT).show();
+                    break;
+                }
+                else {
+                    txt.setText("You choose cycle time : every "+edtxt.getText()+" days~~~");
+                    mMyDialog.dismiss();
+                    break;
+                }
+
+            }
+                default: Toast.makeText(this, "please click on valid button ！", LENGTH_SHORT).show();
         }
     }
+
+
+
+    //日期时间选择：
+    //！！！！！！！！！！--------------------------------------------------------------------------------------------------------------------
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static void showDatePickerDialog(final Activity activity, int themeResId, final TextView tv, final Calendar calendar) {
+        // 直接创建一个DatePickerDialog对话框实例，并将它显示出来
+        new DatePickerDialog(activity, themeResId, new DatePickerDialog.OnDateSetListener() {
+            // 绑定监听器(How the parent is notified that the date is set.)
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // 此处得到选择的时间，可以进行你想要的操作
+                tv.setText("You choose：#Year " + year + " #Month " + (monthOfYear + 1) + " #Day " + dayOfMonth + " ~~~");
+                final TextView txtTime=(TextView)activity.findViewById(R.id.create_timer_datetimelayout_showtime);
+                showTimePickerDialog(activity,  0, txtTime, calendar);
+            }
+        }
+                // 设置初始日期
+                , calendar.get(Calendar.YEAR)
+                , calendar.get(Calendar.MONTH)
+                , calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    /**
+     * 时间选择
+     * @param activity
+     * @param themeResId
+     * @param tv
+     * @param calendar
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static void showTimePickerDialog(final Activity activity, int themeResId, final TextView tv, final Calendar calendar) {
+        // Calendar c = Calendar.getInstance();
+        // 创建一个TimePickerDialog实例，并把它显示出来
+        // 解释一哈，Activity是context的子类
+        new TimePickerDialog( activity,themeResId,
+                // 绑定监听器
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        tv.setText("You choose: #Hour " + hourOfDay + " #Minute " + minute  + " ~~~");
+
+                    }
+                }
+                // 设置初始时间
+                , calendar.get(Calendar.HOUR_OF_DAY)
+                , calendar.get(Calendar.MINUTE)
+                // true表示采用24小时制
+                ,true).show();
+    }
+
+//--------------------------------------------------------------------------------------------------------------------------------------！！！！！！！
+
+
 }
---
+++ app/src/main/java/student/jnu/cockyconan/itimer_copycat/DateTime.java
@@ -0,0 +1,10 @@
+package student.jnu.cockyconan.itimer_copycat;
+
+import java.util.Timer;
+
+class MyTimer {
+    private String Title;
+    private String Note;
+    
+
+}
